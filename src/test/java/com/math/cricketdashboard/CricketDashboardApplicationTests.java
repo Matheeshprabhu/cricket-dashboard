c9cb76fda38287ba01280b9e10d996563769feda
Matheesh <matheeshprabhu123@gmail.com>
2021-12-04 20:18:37 +0530

    Initial commit

---
+++ src/test/java/com/math/cricketdashboard/CricketDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math.cricketdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
